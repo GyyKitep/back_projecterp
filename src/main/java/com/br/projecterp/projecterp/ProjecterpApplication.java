4b090685861edbd56d51b5eb60091ffaf21593aa
Geovanny André Gonsales <geovanny.gonsales.dev@gmail.com>
2026-08-18 22:36:29 -0300

    Início do projeto projecterp

---
+++ src/main/java/com/br/projecterp/projecterp/ProjecterpApplication.java
@@ -0,0 +1,13 @@
+package com.br.projecterp.projecterp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecterpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecterpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/projecterp/projecterp/ProjecterpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projecterp.projecterp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecterpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
